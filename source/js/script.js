8fad20252381fa7b7a47d71a76f446501c6ff7ce
Gang Torchinsky <tor4911@yandex.ru>
2021-05-29 19:35:50 +0300

    Скрипт для карточек на десктопе

---
+++ source/js/script.js
@@ -0,0 +1,17 @@
+const guide = document.querySelector('.guide');
+const guideItems = guide.querySelectorAll('.guide__list-item');
+const guideCards = guide.querySelectorAll('.country-card');
+
+for (let i = 0; i < guideItems.length; i++) {
+  guideItems[i].addEventListener('click', function(evt) {
+    evt.preventDefault();
+    guideItems.forEach(item => {
+      item.classList.remove('guide__list-item--current')
+    })
+    guideCards.forEach(card => {
+      card.classList.add('visually-hidden')
+    })
+    guideItems[i].classList.add('guide__list-item--current');
+    guideCards[i].classList.remove('visually-hidden')
+  })
+}
