00212b6463f00de74230a98270af6b83361ec5d6
Gang Torchinsky <tor4911@yandex.ru>
2021-05-29 19:43:14 +0300

    Скрипт для карточек на десктопе 2

--- source/js/script.js
+++ source/js/script.js
@@ -1,3 +1,4 @@
+const placesBtn = document.querySelectorAll('.places__button');
 const guide = document.querySelector('.guide');
 const guideItems = guide.querySelectorAll('.guide__list-item');
 const guideCards = guide.querySelectorAll('.country-card');
@@ -5,6 +6,17 @@ const guideCards = guide.querySelectorAll('.country-card');
 for (let i = 0; i < guideItems.length; i++) {
   guideItems[i].addEventListener('click', function(evt) {
     evt.preventDefault();
+    guideItems.forEach(item => {
+      item.classList.remove('guide__list-item--current')
+    })
+    guideCards.forEach(card => {
+      card.classList.add('visually-hidden')
+    })
+    guideItems[i].classList.add('guide__list-item--current');
+    guideCards[i].classList.remove('visually-hidden')
+  })
+
+  placesBtn[i].addEventListener('click', function() {
     guideItems.forEach(item => {
       item.classList.remove('guide__list-item--current')
     })
